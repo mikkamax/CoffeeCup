fdd74a698da07099c9bde77ef71d1e0e863025b8
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-01-31 23:31:04 +0700

    Add calculation of price

--- app/src/main/java/com/mike/justjava/MainActivity.java
+++ app/src/main/java/com/mike/justjava/MainActivity.java
@@ -8,10 +8,14 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    private int quantity, price = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
     public void submitOrder(View view) {
-        display(1);
+        quantity++;
+        display(quantity);
+        displayPrice(quantity * price);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(String.valueOf(number));
+    }
+
+    private void displayPrice(int price) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance(Locale.US).format(price));
     }
 }
