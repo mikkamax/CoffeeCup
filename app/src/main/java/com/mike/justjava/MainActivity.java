b43689a5776bd11469c72837be2108c4fe7901e4
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-02-02 15:20:38 +0700

    Add increment/decrement buttons plus functions

--- app/src/main/java/com/mike/justjava/MainActivity.java
+++ app/src/main/java/com/mike/justjava/MainActivity.java
@@ -24,18 +24,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
+        doDisplay();
+    }
+
+    public void doIncrement(View view) {
         quantity++;
+        doDisplay();
+    }
+
+    public void doDecrement(View view) {
+        quantity--;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+        doDisplay();
+    }
+
+    public void doDisplay() {
         display(quantity);
         displayPrice(quantity * price);
     }
 
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText(String.valueOf(number));
     }
 
     private void displayPrice(int price) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance(Locale.US).format(price));
     }
 }
